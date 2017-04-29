8a2b5d23003ec85697ff202adef7a90ae4f5a9da
Aleksey Belchenkov <u608110@gmail.com>
2017-04-29 21:30:06 +0300

    Create File Structure

---
+++ README.md
@@ -0,0 +1,4 @@
+# Create Corporative Site
+
+1. git clone
+2. demo 
---
+++ template/default/js/func.js
@@ -0,0 +1,41 @@
+function show1(){
+	document.getElementById('mlink1').style.background='url(images/bgMenu1h.gif) 23px top  no-repeat';
+	}
+function hide1(){
+	document.getElementById('mlink1').style.background='url(images/bgMenu1.gif) 23px top no-repeat';
+	}
+function show2(){
+	document.getElementById('mlink2').style.background='url(images/bgMenu2h.gif) 23px top  no-repeat';
+	}
+function hide2(){
+	document.getElementById('mlink2').style.background='url(images/bgMenu2.gif) 23px top no-repeat';
+	}	
+function show3(){
+	document.getElementById('mlink3').style.background='url(images/bgMenu3h.gif) 23px top  no-repeat';
+	}
+function hide3(){
+	document.getElementById('mlink3').style.background='url(images/bgMenu3.gif) 23px top no-repeat';
+	}	
+function show4(){
+	document.getElementById('mlink4').style.background='url(images/bgMenu4h.gif) 23px top  no-repeat';
+	}
+function hide4(){
+	document.getElementById('mlink4').style.background='url(images/bgMenu4.gif) 23px top no-repeat';
+	}	
+function showList(){
+	if(document.getElementById('katalog').style.display=='none')
+		document.getElementById('katalog').style.display='block';
+	else
+		document.getElementById('katalog').style.display='none';
+	}	
+
+img1=new Image();
+img1.src="images/bgMenu1h.gifG";	
+img2=new Image();
+img2.src="images/bgMenu2h.gif";
+img3=new Image();
+img3.src="images/bgMenu3h.gif";
+img4=new Image();
+img4.src="images/bgMenu4h.gif";
+
+	
